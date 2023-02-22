beb6139547ed6481ae170b7cab96602667840c0e
RAUL SWANSTON <raul@debian-rs.reffacil.com>
2023-02-21 21:26:18 -0500

    Moviendo archivos al repositorio Primera web

---
+++ global.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('Hola soy una instruccion ejecutada desde javascript');
+
+    // document.getElementById('nav').style.background = "brown";
+    // document.getElementsByTagName('nav')[1].style.background = "lightblue";
+    // document.getElementsByName('footer')[0].style.height = '250px';
+});
