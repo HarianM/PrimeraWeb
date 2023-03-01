9eb03c082e24d21c647ab68ba75c429c9dc7e6c8
RAUL SWANSTON <raul@debian-rs.reffacil.com>
2023-02-28 22:14:58 -0500

    Primera tarea de JavaScript

--- js/global.js
+++ js/global.js
@@ -4,4 +4,13 @@ document.addEventListener('DOMContentLoaded',function(){
     // document.getElementById('nav').style.background = "brown";
     // document.getElementsByTagName('nav')[1].style.background = "lightblue";
     // document.getElementsByName('footer')[0].style.height = '250px';
+
+    /*
+        Selecciona el elemento de lista ordenada o desordenada del documento html.html
+        y edita su color de fondo
+
+        recuerda que la ruta(path) es /view/tutorial/html.html
+    */
+
+    document.getElementById("OrderList").style.background = "yellow";
 });
