43ba0f2076e242b8f550494455f92553b2c882aa
RAUL SWANSTON <raul@debian-rs.reffacil.com>
2023-03-25 15:34:55 -0500

    Introducción a HTML

--- js/global.js
+++ js/global.js
@@ -1,16 +1,24 @@
-document.addEventListener('DOMContentLoaded',function(){
-    console.log('Hola soy una instruccion ejecutada desde javascript');
+if(!sessionStorage.getItem('lang')){
+    sessionStorage.setItem('lang', window.navigator.language.substring(0,2))
+};
 
-    // document.getElementById('nav').style.background = "brown";
-    // document.getElementsByTagName('nav')[1].style.background = "lightblue";
-    // document.getElementsByName('footer')[0].style.height = '250px';
+document.addEventListener('DOMContentLoaded',function(event){
 
-    /*
-        Selecciona el elemento de lista ordenada o desordenada del documento html.html
-        y edita su color de fondo
+    let includeHTML = (element, file) =>  {
+        const path = "view/include/";
+        const xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+        xhr.addEventListener("readystatechange", (event) => {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                element.outerHTML = xhr.responseText;
+            }
+        })
+        xhr.open("GET", path + file);
+        xhr.setRequestHeader("Content-type","text/html; charset=utf-8");
+        xhr.send();
+    }
 
-        recuerda que la ruta(path) es /view/tutorial/html.html
-    */
-
-    document.getElementById("OrderList").style.background = "yellow";
+    document.querySelectorAll("[data-include]").forEach(function(element){
+        includeHTML(element, element.getAttribute("data-include"));
+        console.log(element)
+    });
 });
